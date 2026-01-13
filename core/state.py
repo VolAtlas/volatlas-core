a590918b110c35520a5c7f29e6346e352d40e2d3
Sharesth <sharesth444yt@gmail.com>
2026-01-13 11:23:55 +0530

    added a new class function named as volstate

--- core/state.py
+++ core/state.py
@@ -1,2 +1,12 @@
 from dataclasses import dataclass
 from datetime import datetime
+from sqlite3.dbapi2 import Timestamp
+
+@dataclass
+class Volstate:
+    asset: str
+    timestamp: datetime
+
+    level : float
+    curvature : float 
+    skew : float 
